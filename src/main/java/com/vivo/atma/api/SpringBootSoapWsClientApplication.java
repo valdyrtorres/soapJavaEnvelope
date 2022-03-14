2d32e3bb2bb24cff96d481c08d5fdc2530bd3044
valdir-builders <100855821+valdir-builders@users.noreply.github.com>
2022-03-14 19:13:33 -0300

    commit inicial

---
+++ src/main/java/com/vivo/atma/api/SpringBootSoapWsClientApplication.java
@@ -0,0 +1,30 @@
+package com.vivo.atma.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vivo.atma.api.client.SoapClient;
+import com.vivo.atma.api.loaneligibility.Acknowledgement;
+import com.vivo.atma.api.loaneligibility.CustomerRequest;
+
+@SpringBootApplication
+@RestController
+public class SpringBootSoapWsClientApplication {
+	
+	@Autowired
+	private SoapClient cliente;
+	
+	@PostMapping("getLoanStatus")
+	public Acknowledgement invokeSoapClientToGetLoanStatus(@RequestBody CustomerRequest request) {
+		return cliente.getLoanStatus(request);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapWsClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vivo/atma/api/config/SoapConfig.java
@@ -0,0 +1,18 @@
+package com.vivo.atma.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapConfig {
+	
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setPackagesToScan("com.tools.spring.soap.api.loaneligibility");
+		return marshaller;
+	}
+
+}
